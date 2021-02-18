36da47116cec83fdaea3111bbc88f3c654a52808
ngoquoctruong <ngoquocturong302@gmail.com>
2021-02-18 14:26:16 +0700

    Updated

--- src/settings.f02bf.js
+++
@@ -1,1 +0,0 @@
-window._CCSettings={platform:"web-mobile",groupList:["Objects","Walls","UI"],collisionMatrix:[[true,true],[true,false],[false,false,false]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/scenes/intro.fire",orientation:"portrait",jsList:[],bundleVers:{internal:"279e0",main:"02d56"}};
